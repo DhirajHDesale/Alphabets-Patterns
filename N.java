246a5a64d70a61b46d0a96e5a8f1cd0e5c8c6bc9
DhirajHDesale <dhirajhanumantpatil@gmail.ocm>
2024-09-12 19:16:10 +0530

    N

---
+++ N.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class Pattern22 {
+
+	/*
+	 
+*   *
+**  *
+* * *
+*  **
+*   *
+
+
+ 
+	 */
+	
+	public static void main(String[] args) {
+	int row = 5;
+	for(int i = 1;i<=row;i++) {
+		for(int j = 1;j<=row;j++) {
+			if(i==j || j==row || j==1)
+				System.out.print("*");
+			else
+				System.out.print(" ");
+		}
+		System.out.println();
+	}
+	
+	}
+
+}
