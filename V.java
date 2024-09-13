f43cf1c18da14eb94f6d9da2da8fbc5a1a1d885e
DhirajHDesale <dhirajhanumantpatil@gmail.ocm>
2024-09-13 09:10:06 +0530

    V

---
+++ V.java
@@ -0,0 +1,33 @@
+package practice;
+
+public class Pattern22 {
+
+	/*
+	 
+*     *
+ *   * 
+  * *  
+   *   
+ 
+	 */
+	
+	public static void main(String[] args) {
+		
+		int row = 4;
+		for(int i = 1;i<=row;i++) {
+			for(int j = 1;j<=row;j++) {
+				if(i==j)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			for(int j = 2;j<=row;j++) {
+				if(i+j==row+1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
